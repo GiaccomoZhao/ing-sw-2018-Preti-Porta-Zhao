51749b5ce534dce96852d239bf3759dd95998eb1
1996Davide <davidemaria.porta@mail.polimi.it>
2018-04-15 16:47:42 +0200

    Added an archetype about card classes

--- src/main/java/PorPreZha/Model/PrivateObjectiveCard.java
+++ src/main/java/PorPreZha/Model/PrivateObjectiveCard.java
@@ -1,25 +1,30 @@
 package PorPreZha.Model;
 
 
+import javax.tools.Tool;
 
-public enum class PrivateObjectiveCard extends ObjectiveCard {
+public class PrivateObjectiveCard extends ObjectiveCard {
+
+    public final PRC prc;
+    public enum PRC {
+
+        PRC1("Sfumature Rosse - Privata", 11, "Somma dei valori su tutti i dadi rossi", 0), PRC2("Sfumature Gialle - Privata", 12, "Somma dei valori su tutti i dadi gialli", 0), PRC13("Sfumature Verdi - Privata", 13, "Somma dei valori su tutti i dadi verdi", 0), PRC14("Sfumature Blu - Privata", 14, "Somma dei valori su tutti i dadi blu", 0), PRC15("Sfumature Viola - Privata", 15, "Somma dei valori su tutti i dadi viola", 0);
+        // Notare che i valori dei punteggi sono posti a 0 di default, in sede di calcolo punteggio si effettuerà la valutazione del punteggio attribuito alla data carta
 
 
-    PRC1("Sfumature Rosse - Privata",11,"Somma dei valori su tutti i dadi rossi",0),PRC2("Sfumature Gialle - Privata",12,"Somma dei valori su tutti i dadi gialli",0),PRC13("Sfumature Verdi - Privata",13,"Somma dei valori su tutti i dadi verdi",0),PRC14("Sfumature Blu - Privata",14,"Somma dei valori su tutti i dadi blu",0),PRC15("Sfumature Viola - Privata",15,"Somma dei valori su tutti i dadi viola",0);
+        PRC(String name, int ID, String description, int numScore) {
 
-    // Notare che i valori dei punteggi sono posti a 0 di default, in sede di calcolo punteggio si effettuerà la valutazione del punteggio attribuito alla data carta
+            this.name = name;
+            this.ID = ID;
+            this.description = description;
+            this.numScore = numScore;
+
+        }
+
+    }
 
 
-
-    PrivateObjectiveCard(String name, int ID, String description, int numScore){
-
-        this.name=name;
-
-        this.ID=ID;
-
-        this.description=description;
-
-        this.numScore=numScore;
-
+    public PrivateObjectiveCard(PRC prc){
+        this.prc=prc;
     }
 }
--- src/main/java/PorPreZha/Model/ToolCard.java
+++ src/main/java/PorPreZha/Model/ToolCard.java
@@ -7,26 +7,27 @@ import java.util.List;
 
 
 
-public enum class ToolCard extends Card{
+public class ToolCard extends Card{
 
     public int tokensQuantity;
+    public final TC tc;
+    public enum TC {
 
+        TC1("Pinza Sgrossatrice", 16, "Dopo aver scelto un dado,\naumenta o dominuisci il valore\ndel dado scelto di 1\nNon puoi cambiare\nun 6 in 1 o un 1 in 6 ", 0, PURPLE), TC2("Pinza per Eglomise", 17, "Muovi un qualsiasi dado nella tua\nvetrata ignorando le restrizioni\ndi colore\nDevi rispettare tutte le altre\nrestrizioni di piazzamento", 0, BLUE), TC3("Alesatore per lamina di rame", 18, "Muovi un qualsiasi dado nella tua\nvetrata ignorando le restrizioni\ndi valore\nDevi rispettare tutte le altre\nrestrizioni di piazzamento", 0, RED), TC4("Lathekin", 19, "Muovi esattamente due dadi,\nrispettando tutte le restrizioni di\npiazzamento", 0, YELLOW), TC5("Taglierina circolare", 20, "Dopo aver scelto un dado,\nscambia quel dado con un dado\nsul Tracciato dei Round", 0, GREEN), TC6("Pennello per Pasta Salda", 21, "Dopo aver scelto un dado, tira\nnuovamente quel dado\nSe non puoi piazzarlo, riponilo\nnella Riserva", 0, PURPLE), TC7("Martelletto", 22, "Tira nuovamente\ntutti i dadi della Riserva\nQuesta carta può essera usata\nsolo durante il tuo secondo turno,\nprima di scegliere il secondo dado", 0, BLUE), TC8("Tenaglia a Rotelle", 23, "Dopo il tuo primo turno scegli\nimmediatamente un altro dado\nSalta il tuo secondo turno in\nquesto round", 0, RED), TC9("Riga in Sughero", 24, "Dopo aver scelto un dado,\npiazzalo in una casella che non\nsia adiacente a un altro dado\nDevi rispettare tutte le restrizioni\ndi piazzamento", 0, YELLOW), TC10("Tampone Diamantato", 25, "Dopo aver scelto un dado, giralo\nsulla faccia opposta\n6 diventa 1, 5 diventa 2, 4\ndiventa 3 ecc.", 0, GREEN), TC11("Diluente per Pasta Salda", 26, "Dopo aver scelto un dado, riponilo nel\nSacchetto, poi pescane uno dal Sacchetto\nScegli il valore del nuovo dado e\npiazzalo, rispettando tutte le restrizioni di\npiazzamento", 0, PURPLE), TC12("Taglierina Manuale", 27, "Muovi fino a due dadi dello\nstesso colore di un solo dado sul\nTracciato dei Round\nDevi rispettare tutte le restrizioni\ndi piazzamento", 0, BLUE), TC13("Strip Cutter", 28, "Take one die from any player. Give them\na die of matching Color or Value\nThey may place it ignoring Color or\nValue Restrictions\nMay only be used before Round 7", 0, PURPLE);
 
-    TC1("Pinza Sgrossatrice",16,"Dopo aver scelto un dado,\naumenta o dominuisci il valore\ndel dado scelto di 1\nNon puoi cambiare\nun 6 in 1 o un 1 in 6 ",0,PURPLE),TC2("Pinza per Eglomise",17,"Muovi un qualsiasi dado nella tua\nvetrata ignorando le restrizioni\ndi colore\nDevi rispettare tutte le altre\nrestrizioni di piazzamento",0,BLUE),TC3("Alesatore per lamina di rame",18,"Muovi un qualsiasi dado nella tua\nvetrata ignorando le restrizioni\ndi valore\nDevi rispettare tutte le altre\nrestrizioni di piazzamento",0,RED),TC4("Lathekin",19,"Muovi esattamente due dadi,\nrispettando tutte le restrizioni di\npiazzamento",0,YELLOW),TC5("Taglierina circolare",20,"Dopo aver scelto un dado,\nscambia quel dado con un dado\nsul Tracciato dei Round",0,GREEN),TC6("Pennello per Pasta Salda",21,"Dopo aver scelto un dado, tira\nnuovamente quel dado\nSe non puoi piazzarlo, riponilo\nnella Riserva",0,PURPLE),TC7("Martelletto",22,"Tira nuovamente\ntutti i dadi della Riserva\nQuesta carta può essera usata\nsolo durante il tuo secondo turno,\nprima di scegliere il secondo dado",0,BLUE),TC8("Tenaglia a Rotelle",23,"Dopo il tuo primo turno scegli\nimmediatamente un altro dado\nSalta il tuo secondo turno in\nquesto round",0,RED),TC9("Riga in Sughero",24,"Dopo aver scelto un dado,\npiazzalo in una casella che non\nsia adiacente a un altro dado\nDevi rispettare tutte le restrizioni\ndi piazzamento",0,YELLOW),TC10("Tampone Diamantato",25,"Dopo aver scelto un dado, giralo\nsulla faccia opposta\n6 diventa 1, 5 diventa 2, 4\ndiventa 3 ecc.",0,GREEN),TC11("Diluente per Pasta Salda",26,"Dopo aver scelto un dado, riponilo nel\nSacchetto, poi pescane uno dal Sacchetto\nScegli il valore del nuovo dado e\npiazzalo, rispettando tutte le restrizioni di\npiazzamento",0,PURPLE),TC12("Taglierina Manuale",27,"Muovi fino a due dadi dello\nstesso colore di un solo dado sul\nTracciato dei Round\nDevi rispettare tutte le restrizioni\ndi piazzamento",0,BLUE),TC13("Strip Cutter",28,"Take one die from any player. Give them\na die of matching Color or Value\nThey may place it ignoring Color or\nValue Restrictions\nMay only be used before Round 7",0,PURPLE);
+        TC(String name, int ID, String description, int tokensQuantity, Dice.ColorDice colorDice) {
 
-    ToolCard(String name,int ID,String description,int tokensQuantity, Dice.ColorDice colorDice){
-
-        this.name=name;
-
-        this.ID=ID;
-
-        this.description=description;
-
-        this.tokensQuantity=tokensQuantity;
-        
-        this.colorDice=colorDice;
+            this.name = name;
+            this.ID = ID;
+            this.description = description;
+            this.tokensQuantity = tokensQuantity;
+            this.colorDice = colorDice;
+        }
     }
 
+    public ToolCard(TC tc){
+        this.tc=tc;
+    }
 
     public void use(){};
 
