cca0ec63677515448b71640daf95b35b369971c0
1996Davide <davidemaria.porta@mail.polimi.it>
2018-04-22 20:27:34 +0200

    Fixed a public objective card effect

--- src/main/java/porprezha/model/cards/PublicObjectiveCard.java
+++ src/main/java/porprezha/model/cards/PublicObjectiveCard.java
@@ -215,7 +215,7 @@ public class PublicObjectiveCard extends ObjectiveCard{
 
                   }
               }
-
+              
               for(i=0;i<4;i++){
                   for(j=0;j<5;j++){
 
