4fade30dff8cc879bb6eb62d3ff02c456f8bceac
ZhaoX <10454297@polimi.it>
2018-06-09 11:20:04 +0200

    delete .idea

--- src/main/java/porprezhas/view/fx/gameScene/GuiSettings.java
+++ src/main/java/porprezhas/view/fx/gameScene/GuiSettings.java
@@ -8,7 +8,7 @@ import javafx.beans.property.SimpleDoubleProperty;
 import java.io.File;
 
 public class GuiSettings {
-    public static boolean bFixedFont = true;
+    public static boolean bFixedFont = false;
 
     // ***** Game GUI attributes *****
     // path
