64139f3a12561623cf88d93c6bd7a7c5201bc76c
1996Davide <davidemaria.porta@mail.polimi.it>
2018-07-04 18:00:57 +0200

    improved the coverage of Objective Card classes

--- src/test/java/porprezhas/model/cards/PublicObjectiveCardTest.java
+++ src/test/java/porprezhas/model/cards/PublicObjectiveCardTest.java
@@ -5,7 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import porprezhas.model.dices.Board;
 import porprezhas.model.dices.Dice;
-
+import porprezhas.model.dices.Pattern;
 
 
 import static org.junit.Assert.assertEquals;
@@ -16,100 +16,136 @@ import static org.mockito.Mockito.when;
 public class PublicObjectiveCardTest {
 
 
-    Dice die0, die1, die2, die3, die4,die5,die6,die7,die8,dieNull;
+    Dice die0, die1, die2, die3, die4, die5, die6, die7, die8, die9, die10;
     Board board;
     PublicObjectiveCard publicObjectiveCard1;
+    PublicObjectiveCard publicObjectiveCard2;
+    PublicObjectiveCard publicObjectiveCard3;
+    PublicObjectiveCard publicObjectiveCard4;
+    PublicObjectiveCard publicObjectiveCard5;
+    PublicObjectiveCard publicObjectiveCard6;
+    PublicObjectiveCard publicObjectiveCard7;
     PublicObjectiveCard publicObjectiveCard8;
+    PublicObjectiveCard publicObjectiveCard9;
+    PublicObjectiveCard publicObjectiveCard10;
     Card.Effect effect;
 
 
-
-
-
     @Before
-    public void setUp()  {
+    public void setUp() {
 
-        die0 = mock(Dice.class);
-        die1 = mock(Dice.class);
-        die2 = mock(Dice.class);
-        die3 = mock(Dice.class);
-        die4 = mock(Dice.class);
-        die5 = mock(Dice.class);
-        die6 = mock(Dice.class);
-        die7 = mock(Dice.class);
-        die8 = mock(Dice.class);
-        dieNull = mock(Dice.class);
-        board = mock(Board.class);
-
-
+        die0 = new Dice(Dice.ColorDice.PURPLE,5,0);
+        die1 = new Dice(Dice.ColorDice.BLUE,3,1);
+        die2 = new Dice(Dice.ColorDice.RED,2,2);
+        die3 = new Dice(Dice.ColorDice.GREEN,1,3);
+        die4 = new Dice(Dice.ColorDice.YELLOW,4,4);
+        die5 = new Dice(Dice.ColorDice.PURPLE,4,5);
+        die6 = new Dice(Dice.ColorDice.YELLOW,3,6);
+        die7 = new Dice(Dice.ColorDice.RED,5,7);
+        die8 = new Dice(Dice.ColorDice.GREEN,6,8);
+        die9 = new Dice(Dice.ColorDice.BLUE,5,9);
+        die10 = new Dice(Dice.ColorDice.GREEN,1,10);
 
 
+        board = new Board(Pattern.TypePattern.VOID);
 
-        publicObjectiveCard1 = new PublicObjectiveCard(Card.Effect.PUC5);
+
+        publicObjectiveCard1 = new PublicObjectiveCard(Card.Effect.PUC1);
+        publicObjectiveCard2 = new PublicObjectiveCard(Card.Effect.PUC2);
+        publicObjectiveCard3 = new PublicObjectiveCard(Card.Effect.PUC3);
+        publicObjectiveCard4 = new PublicObjectiveCard(Card.Effect.PUC4);
+        publicObjectiveCard5 = new PublicObjectiveCard(Card.Effect.PUC5);
+        publicObjectiveCard6 = new PublicObjectiveCard(Card.Effect.PUC6);
+        publicObjectiveCard7 = new PublicObjectiveCard(Card.Effect.PUC7);
         publicObjectiveCard8 = new PublicObjectiveCard(Card.Effect.PUC8);
+        publicObjectiveCard9 = new PublicObjectiveCard(Card.Effect.PUC9);
+        publicObjectiveCard10 = new PublicObjectiveCard(Card.Effect.PUC10);
 
 
-        when(die0.getDiceNumber()).thenReturn(1);
-        when(die1.getDiceNumber()).thenReturn(2);
-        when(die2.getDiceNumber()).thenReturn(5);
-        when(die3.getDiceNumber()).thenReturn(2);
-        when(die4.getDiceNumber()).thenReturn(4);
-        when(die5.getDiceNumber()).thenReturn(1);
-        when(die6.getDiceNumber()).thenReturn(6);
-        when(die7.getDiceNumber()).thenReturn(4);
-        when(die8.getDiceNumber()).thenReturn(3);
-        when(dieNull.getDiceNumber()).thenReturn(0);
-
-        when(die0.getColorDice()).thenReturn(Dice.ColorDice.RED);
-        when(die1.getColorDice()).thenReturn(Dice.ColorDice.BLUE);
-        when(die2.getColorDice()).thenReturn(Dice.ColorDice.BLUE);
-        when(die3.getColorDice()).thenReturn(Dice.ColorDice.YELLOW);
-        when(die4.getColorDice()).thenReturn(Dice.ColorDice.YELLOW);
-        when(die5.getColorDice()).thenReturn(Dice.ColorDice.GREEN);
-        when(die6.getColorDice()).thenReturn(Dice.ColorDice.PURPLE);
-        when(die7.getColorDice()).thenReturn(Dice.ColorDice.RED);
-        when(die8.getColorDice()).thenReturn(Dice.ColorDice.GREEN);
-        when(die8.getColorDice()).thenReturn(Dice.ColorDice.WHITE);
-
-
-        when(board.getDice(0,0)).thenReturn(die1);
-        when(board.getDice(0,1)).thenReturn(die0);
-        when(board.getDice(0,2)).thenReturn(dieNull);
-        when(board.getDice(0,3)).thenReturn(dieNull);
-        when(board.getDice(0,4)).thenReturn(dieNull);
-        when(board.getDice(1,0)).thenReturn(die5);
-        when(board.getDice(1,1)).thenReturn(die2);
-        when(board.getDice(1,2)).thenReturn(die3);
-        when(board.getDice(1,3)).thenReturn(die6);
-        when(board.getDice(1,4)).thenReturn(die7);
-        when(board.getDice(2,0)).thenReturn(dieNull);
-        when(board.getDice(2,1)).thenReturn(die4);
-        when(board.getDice(2,2)).thenReturn(dieNull);
-        when(board.getDice(2,3)).thenReturn(dieNull);
-        when(board.getDice(2,4)).thenReturn(dieNull);
-        when(board.getDice(3,0)).thenReturn(dieNull);
-        when(board.getDice(3,1)).thenReturn(die8);
-        when(board.getDice(3,2)).thenReturn(dieNull);
-        when(board.getDice(3,3)).thenReturn(dieNull);
-        when(board.getDice(3,4)).thenReturn(dieNull);
-
-
-
-
+        board.insertDice(die0,0,0);
+        board.insertDice(die1,0,1);
+        board.insertDice(die2,0,2);
+        board.insertDice(die3,0,3);
+        board.insertDice(die4,0,4);
+        board.insertDice(die5,1,1);
+        board.insertDice(die6,1,2);
+        board.insertDice(die7,1,3);
+        board.insertDice(die8,1,4);
+        board.insertDice(die9,2,2);
+        board.insertDice(die10,3,2);
 
     }
 
 
     @Test
-    public void applyTest(){
+    public void applyCard1Test() {
 
-        assertEquals(publicObjectiveCard1.apply(board),4);
-
-        assertEquals(publicObjectiveCard8.apply(board),5);
-
+        assertEquals(publicObjectiveCard1.apply(board),6);
 
     }
 
+    @Test
+    public void applyCard2Test() {
 
+        assertEquals(publicObjectiveCard2.apply(board), 5);
+
+    }
+
+    @Test
+    public void applyCard3Test() {
+
+        assertEquals(publicObjectiveCard3.apply(board), 5);
+
+    }
+
+    @Test
+    public void applyCard4Test() {
+
+        assertEquals(publicObjectiveCard4.apply(board), 4);
+
+    }
+
+    @Test
+    public void applyCard5Test() {
+
+        assertEquals(publicObjectiveCard5.apply(board), 2);
+
+    }
+
+    @Test
+    public void applyCard6Test() {
+
+        assertEquals(publicObjectiveCard6.apply(board), 4);
+
+    }
+
+    @Test
+    public void applyCard7Test() {
+
+        assertEquals(publicObjectiveCard7.apply(board), 2);
+
+    }
+
+    @Test
+    public void applyCard8Test() {
+
+        assertEquals(publicObjectiveCard8.apply(board), 5);
+
+    }
+
+    @Test
+    public void applyCard9Test() {
+
+        assertEquals(publicObjectiveCard9.apply(board), 6);
+
+    }
+
+    @Test
+    public void applyCard10Test() {
+
+        assertEquals(publicObjectiveCard10.apply(board), 8);
+
+
+    }
 }
 
